09069740e767c1d41bd18f3e549548225c4ffcca
umar260 <112659543+umar260@users.noreply.github.com>
2022-09-15 14:41:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,16 @@
 # Main Program Loop
-    def (main):
-        list =[Umar, Ali, Sam]
-        print(list)
+def main():
+        Name =["John", "Ben" , "Sam"]
+        print(Name)
 
-    loop = True
-    while loop:
-        selection = getMenuSelection()
-
-        if selection == "Umar":
-            printUmars(Umar)
-        elif selection == "Ali":
-
-        elif selection == "3":
-            
-        elif selection == "4":
-            
-        elif selection == "5":
-            loop = False
-        else:
-            print("Invalid selection")
-
-    def getMenuSelection():
+    
+        
+def getMenuSelection():
     print("\nMAIN MENU")
-    print("1: Display All Grades")
-    print("2: Display Honours")
-    print("3: Stats")
-    print("4: Randomize Grades")
-    print("5: Exit")
+    print("1: Display All Contact Names")
+    print("2: Search for Contact")
+    print("3: Edit Contact")
+    print("4: New Contact")
+    print("5: Remove contact")
 
-    def printUmar(Umars):
-        print("\nAll information about contact name Umar")
-        for Umar in  Umars:
-            print("Name = Umar")
-            print("Phone#= 0301447038")
-            print("email = umar780@Yahoo.com")
-
+    
