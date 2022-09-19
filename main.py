5c5c07bb73a32e415be2767afabfae97c7cca860
umar260 <112659543+umar260@users.noreply.github.com>
2022-09-19 14:46:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 # Main Program Loop
-Name =["John", "Ben" , "Sam"]
-    print(Name)
-
-    
-        
 def getMenuSelection():
     print("\nMAIN MENU")
-    print("1: Display All Contact Names")
-    print("2: Search for Contact")
-    print("3: Edit Contact")
-    print("4: New Contact")
-    print("5: Remove contact")
+    print("1:Display All Contact Names")
+    print("2:Search for Contact")
+    print("3:Edit Contact")
+    print("4:New Contact")
+    print("5:Remove contact")
+    return str(input("Enter your selection:"))
 
-    
+
+users = [{
+        "name": "Ben", 
+        "phone": "555-5555", 
+        "email": "Ben@mail.com"
+    }, {
+        "name": "Mr. V", 
+        "phone": "444-4444", 
+        "email": "mrv@mail.com"
+    }, {
+        "name": "Mr. Sam", 
+        "phone": "444-4444", 
+        "email": "Sam@mail.com"
+    }];    
+print(users)
+
+for i in range(len(users)):
+    print(users[i]["name"])
+x=getMenuSelection()
+if x=="2":
+    y=input("Enter the Name of the contact you want to search for:")
+    if y in users:
+        print(users[y])
+    else:
+        print("Contact not found")
