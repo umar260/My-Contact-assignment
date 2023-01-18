cd644ae10527c1ae56fd13aa98f3cea0d5d1a722
umar260 <m.zafar9@share.epsb.ca>
2023-01-18 00:24:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # Main Program Loop
 def getMenuSelection():
     print("\nMAIN MENU")
@@ -6,6 +7,7 @@ def getMenuSelection():
     print("3:Edit Contact")
     print("4:New Contact")
     print("5:Remove contact")
+    print("6:Exit")
     return str(input("Enter your selection:"))
 
 users = [{
@@ -25,56 +27,52 @@ users = [{
 while True:
     selection = getMenuSelection()
     if selection == "1":
-        for i in range(len(users)):
-            print(users[i]["name"])
-    elif selection == "2":
-        search_name = input("Enter the Name of the contact you want to search for:")
-        for user_dict in users:
-            if search_name == user_dict["name"]:
-                user_dict["name"]
-                user_dict["phone"]
-                user_dict["email"]
-                print(user_dict)
+        for contact in users:
+            print(contact["name"])
+
+    elif selection=="2":
+        print("Search for contact")
+        name_search=input("Who's contact name do you want to search?: ")
+        for i in users:
+            if i["name"] == name_search:
+                print("")
+                print(i["name"])
+                print(i["phone"])
+                print(i["email"])
                 break
-            print("Contact not found")
-            break
+        else:
+            print("This name doesn't exist in contact list.")
+
     elif selection == "3":
-        search_name = str(input("Enter the Name of the contact you want to edit:"))
-        for user_dict in users:
-            if search_name == user_dict["name"]:
-                user_dict["name"] = input("Enter the new name of the contact:")
-                user_dict["phone"] = input("Enter new phone number:")
-                user_dict["email"] = input("Enter new email:")
-                print("Contact edited")
+        print("Edit contact")
+        editname = input("Who's contact info do you want to change?: ")
+        for i in users:
+            if i["name"] == editname:
+                editednumber = input("What is the new number?: ")
+                i["phone"] = editednumber
+                edited_email = input("What is the new email?: ")
+                i["email"] = edited_email
                 break
-            print("Contact not found")
-            break
-    elif selection == "4":
-        search_name = input("Enter the Name of the contact you want to add:")
-        for user_dict in users:
-            if search_name in user_dict["name"]:
-                print("Contact already exists")
+        else:
+            print("This name doesn't exist in contact list.")
+
+    elif selection=="4":
+        print("New contact")
+        newname = str(input("What is the name of your new contact?: "))
+        newnumber = str(input("What is the number of your new contact?: "))
+        newemail = str(input("What is the email of your new contact?: "))
+        users.append({"name": newname, "phone": newnumber, "email": newemail})
+
+    elif selection=="5":
+        print("Remove Contact")
+        removename=input("Which person name do you want to remove from contact list?: ")
+        for i in users:
+            if i["name"] == removename:
+                users.remove(i)
                 break
-            else:
-                phone = input("Enter the phone number of the contact:")
-                email = input("Enter the email of the contact:")
-                users.append({"name": list, "phone": phone, "email": email})
-                print("Contact added")
-                break
-    elif selection == "5":
-        Name_remove = input("Enter the Name of the contact you want to remove:")
-        for user_dict in users:
-            if Name_remove in user_dict["name"]:
-                users.remove(user_dict)
-                print("Contact removed")
-                break
-            print("Contact not found")
-            break
-    else:
-        selection = 0
-        break
+        else:
+            print("This name doesn't exist in contact list.")
 
-
-print(users)
-
-
+    elif selection == "6":
+         print("EXITED")
+         loop = False
